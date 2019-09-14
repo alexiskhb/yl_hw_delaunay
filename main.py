559d6cdf488c05fed316393a8b589d9872dbb768
alexiskhb <alexiskhb@gmail.com>
2019-09-14 14:36:45 +1000

    Draw Voronoi diagram for added points

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QPaintEvent, QMouseEvent
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.uic import loadUi
-from typing import List, Optional
+from typing import List, Optional, Tuple
+from scipy.spatial import Voronoi
+from scipy.spatial.qhull import QhullError
+import numpy as np
 
 
 class PointSet:
     class PointDescriptor:
-        radius = 5
+        radius = 8
         color = Qt.white
 
         def __init__(self, ps, idx: int):
@@ -38,8 +41,23 @@ class PointSet:
             if isinstance(y, (int, float)):
                 self.xy[1] = y
 
+    class RegionDescriptor:
+        def __init__(self, ps, prr: Tuple[int, List[Tuple[int, List[float]]]]):
+            self.ps = ps
+            self.idx = prr[0]
+            self.ngbrs = [p for p, _ in prr[1]]
+            self.ridges = [((int(x1), int(y1)), (int(x2), int(y2))) for _, ((x1, y1), (x2, y2)) in prr[1]]
+
+        def draw(self, painter):
+            if isinstance(painter, QPainter):
+                for i, ((x1, y1), (x2, y2)) in enumerate(self.ridges):
+                    if self.idx > self.ngbrs[i]:
+                        painter.setPen(Qt.black)
+                        painter.drawLine(x1, y1, x2, y2)
+
     def __init__(self, points=None):
         self.points: List[List[int]] = []
+        self.vor = None
         try:
             self.points.extend(points)
         except TypeError:
@@ -60,20 +78,61 @@ class PointSet:
             if (x - x0)**2 + (y - y0)**2 <= PointSet.PointDescriptor.radius**2:
                 return self[i]
         return None
+    
+    @property
+    def regions(self):
+        try:
+            self.vor = Voronoi(self.points)
+        except QhullError:
+            pass
+        prrs = get_segments(self.vor)
+        return map(lambda prr: PointSet.RegionDescriptor(self, prr), prrs.items())
 
     def draw(self, painter):
-        if isinstance(painter, QPainter):
-            for point in self:
-                point.draw(painter)
+        for region in self.regions:
+            region.draw(painter)
+        for point in self:
+            point.draw(painter)
+
+
+# Копипаста из scipy.spatial.voronoi_plot_2d
+def get_segments(vor):
+    center = vor.points.mean(axis=0)
+    ptp_bound = vor.points.ptp(axis=0)
+    point_region_ridges = {}
+    segments = []
+    for pointidx, simplex in zip(vor.ridge_points, vor.ridge_vertices):
+        simplex = np.asarray(simplex)
+        if np.all(simplex >= 0):
+            segments.append(vor.vertices[simplex])
+        else:
+            i = simplex[simplex >= 0][0]  # finite end Voronoi vertex
+
+            t = vor.points[pointidx[1]] - vor.points[pointidx[0]]  # tangent
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])  # normal
+
+            midpoint = vor.points[pointidx].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[i] + direction * ptp_bound.max()
+
+            segments.append([vor.vertices[i], far_point])
+        for p1, p2 in pointidx, reversed(pointidx):
+            if p1 not in point_region_ridges:
+                point_region_ridges[p1] = []
+            point_region_ridges[p1].append((p2, segments[-1]))
+    return point_region_ridges
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('main.ui')
-        self.ps = PointSet()
+        w0, h0 = self.width()//4, self.height()//4
+        self.ps = PointSet([[w0, h0], [w0, h0*3], [w0*3, h0*3]])
         self.moving = False
         self.current_point = None
+        self.repaint()
 
     def paintEvent(self, event: QPaintEvent):
         painter = QPainter()
