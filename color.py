18a0deb4eeb7071a2cadcd68813065ced508987b
alexiskhb <alexiskhb@gmail.com>
2019-09-14 23:09:49 +1000

    Fixed removing a point with colors off

--- color.py
+++ color.py
@@ -8,15 +8,14 @@ class Color:
 
     @staticmethod
     def get(idx):
-        if not Settings.colors_on:
-            return Settings.default_brush
-
         if idx >= len(Color._idx2clr):
             while len(Color._idx2clr) <= idx:
                 h = 360 * random()
                 s = 80 + 20 * random()
                 l = 95 + 5 * random()
                 Color._idx2clr.append(QColor.fromHsl(h, s, l))
+        if not Settings.colors_on:
+            return Settings.default_brush
         return Color._idx2clr[idx]
 
     @staticmethod
